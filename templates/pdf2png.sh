88ae723df63f6c919491067390f43f08c33bda45
faraz <fahmed02@icloud.com>
2024-11-26 19:31:28 -0500

    Update pdf2png.sh

--- templates/pdf2png.sh
+++ templates/pdf2png.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# for i in *.pdf 
-# do 
-# 	iSUB=`echo $i | cut -d "." -f3` 
-# 	convert -density 300 $i ${iSUB}.png
-
-# done
-
-
-
 convert -density 300 *.geneBodyCoverage.curves.pdf curves.png
 convert -density 300 *.geneBodyCoverage.heatMap.pdf heatMap.png
 
