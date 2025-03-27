823fe18bb433f73cad7625ae47504d096aac79b2
faraz <33409348+bixBeta@users.noreply.github.com>
2025-03-27 02:23:35 -0400

    Update makeReport.sh

--- templates/makeReport.sh
+++ templates/makeReport.sh
@@ -42,7 +42,7 @@ knit_full_filtered(){
 
 knit_nogbcov(){
 
-  cp ${projectDir}/templates/qmds/nogbcov-report-nf.qmd 
+  cp ${projectDir}/templates/qmds/nogbcov-report-nf.qmd .
   
   quarto render nogbcov-report-nf.qmd -P title:${id} -P genome:${genome} -P annot:${annots} -o ${id}-Report.html
 
